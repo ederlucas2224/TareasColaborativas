5a51d7890a58b89d8af312bc3aedafcaa187b36e
Eder <ruizgomez37@gmail.com>
2025-10-26 16:13:21 -0600

    Se sube la api y BD necesaria

---
+++ Data/SqlConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Data
+{
+	public class SqlConnectionFactory : IDbConnectionFactory
+	{
+		private readonly string _connectionString;
+		public SqlConnectionFactory(IConfiguration configuration)
+		{
+			_connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
+
+		public IDbConnection CreateConnection()
+			=> new SqlConnection(_connectionString);
+	}
+}
---
+++ Model/Logs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+	public class Logs
+	{
+		public Guid TaskId { get; set; }
+		public string? PreviousStatus { get; set; }
+		public string? NewStatus { get; set; }
+		public string? ChangedBy { get; set; }
+		public DateTime Timestamp { get; set; }
+	}
+}
